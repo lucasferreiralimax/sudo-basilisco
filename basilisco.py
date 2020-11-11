a709476c69d0e102df3f2a231153f181e7b29b29
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-10 21:21:33 -0300

    example basic json return

--- basilisco.py
+++ basilisco.py
@@ -1,7 +1,23 @@
-from flask import Flask
+#!/usr/bin/env python
+#encoding: utf-8
+# -*- coding: utf-8 -*-
+
+from flask import Flask, json, Response
 
 app = Flask(__name__)
 
+data = {
+    "username": "basilisco",
+    "theme": "Câmara Secreta"
+}
+
 @app.route('/')
 def basilisco():
-    return 'Hello, World!'
+    return 'Do you saying with old snakes?'
+
+@app.route("/basilisco")
+def basilisco_say():    
+    json_string = json.dumps(data,ensure_ascii = False)
+    #creating a Response object to set the content type and the encoding
+    response = Response(json_string,content_type="application/json; charset=utf-8")
+    return response
