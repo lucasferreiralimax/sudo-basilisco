5956d1822bcd8193375d080a6a19db98de986882
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-12 02:39:13 -0300

    adjustment post with code 500

--- basilisco.py
+++ basilisco.py
@@ -2,22 +2,34 @@
 #encoding: utf-8
 # -*- coding: utf-8 -*-
 
-from flask import Flask, json, Response
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
-data = {
-    "username": "basilisco",
-    "theme": "Câmara Secreta"
-}
+data = [
+    {
+        "username": "basilisco",
+        "status": "hidden secrect",
+        "theme": "Câmara Secreta",
+        "age": "XV"
+    }
+]
+
+def basilisco_say():
+    response = make_response(jsonify(data), 201)
+    return response
+
+def basilisco_eye():
+    response = make_response(jsonify({"error": "hidden eye"}), 500)
+    return response
 
 @app.route('/')
-def basilisco():
+def basilisco_home():
     return 'Do you saying with old snakes?'
 
-@app.route("/basilisco")
-def basilisco_say():    
-    json_string = json.dumps(data,ensure_ascii = False)
-    #creating a Response object to set the content type and the encoding
-    response = Response(json_string,content_type="application/json; charset=utf-8")
-    return response
+@app.route("/basilisco", methods=['GET', 'POST'])
+def basilisco_api():    
+    if request.method == 'POST':
+        return basilisco_eye()
+    else:
+        return basilisco_say()
